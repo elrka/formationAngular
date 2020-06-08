9e4d3f4d68781070f91d1ffa6c13f4796e43ff55
elrka <vincentjenni@gmail.com>
2020-06-08 16:59:24 +0200

    MiseEnPlaceDeNgContent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent, HeaderComponent, NavComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [FooterComponent, HeaderComponent, NavComponent]
 })
 export class CoreModule { }
