79407684cc85a2d7b1f427466a319999da0d6ef2
elrka <vincentjenni@gmail.com>
2020-06-09 12:05:11 +0200

    suiteRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { PageLoginComponent } from './login/pages/page-login/page-login.component';
 
 
 const routes: Routes = [
  {   path:"login", component: PageLoginComponent},
- 
+ {
+  path: 'orders',
+  loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule),
+},
+{
+  path: "clients",
+  loadChildren: () => import('./clients/clients.module').then(m =>m.ClientsModule)
+},
+{
+  path: "**",
+  loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule),
+}
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing :true,
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
