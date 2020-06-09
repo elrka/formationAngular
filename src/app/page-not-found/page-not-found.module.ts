b8ae57539cc0276cb3abce2696c9e01c95f3850d
elrka <vincentjenni@gmail.com>
2020-06-09 11:54:49 +0200

    ajoutRouteur

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PageNotFoundRoutingModule } from './page_not_found-routing.module';
 
 
 
 @NgModule({
   declarations: [PageNotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PageNotFoundRoutingModule
   ]
 })
 export class PageNotFoundModule { }
